5162377757bb6a06b5893bbe5fa501fba007dab7
Jared Diaz <145868628+JaredD221@users.noreply.github.com>
2024-12-07 04:37:45 +0000

    updated astro configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://JaredD221.github.io/JaredD221.github.io',
+  outDir: 'dist',
+});
